254361a6cf308197f205654ae06a50d2cac90d84
Dabokwohss <99487961+Dabokwohss@users.noreply.github.com>
2022-02-12 13:51:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+
+# 安装关机功能 集成到系统源码菜单中 不需要另外选择设置即可使用
+
+curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+curl -fsSL  https://raw.githubusercontent.com/sirpdboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
